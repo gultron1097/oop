6ac0a374036aa8e8a7eb39e89c47a9c9aa8ac613
gultron <gultron10970@gmail.com>
2020-03-24 17:20:52 +0800

    Initial code of poker demo

---
+++ poker_demo/src/poker_demo/DisplayPoker.java
@@ -0,0 +1,51 @@
+package poker_demo;
+
+import java.awt.FlowLayout;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class DisplayPoker {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub	
+		//step 1: Generate a random filename
+		int i = (int) (Math.random()*51) + 1;
+		String cardFilename = "img/" + i + ".png";
+		
+		
+		//Step 2: Load the image from the file
+		//according to the cardFilename
+		
+		//Create a file object
+		File pokerFile = new File(cardFilename);
+		//Read image data from the file object
+		BufferedImage img  = ImageIO.read(pokerFile);
+		
+		//Step 3: Create a GUI interface
+		//for displaying the image
+		ImageIcon icon = new ImageIcon(img);
+		
+		JFrame frame = new JFrame();
+		frame.setLayout(new FlowLayout());
+		frame.setSize(250, 360);
+		frame.setTitle(cardFilename);
+		
+		JLabel lbl = new JLabel();
+		lbl.setIcon(icon);
+		JLabel lbl2 = new JLabel();
+		lbl2.setIcon(icon);
+		
+		frame.add(lbl);
+		frame.add(lbl2);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	}
+
+}
